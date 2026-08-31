6c0c4d8e12e7dc0be67b575c0b905db667ff0071
MrrPsy <psyzombie05@gmail.com>
2026-08-31 22:55:25 +0700

    update

--- app/Http/Controllers/admin/CourseController.php
+++ app/Http/Controllers/admin/CourseController.php
@@ -33,7 +33,7 @@ class CourseController extends Controller
 
         if ($programId) {
             $query->whereHas('programs', function ($pq) use ($programId) {
-                $pq->where('id', $programId);
+                $pq->where('programs.id', $programId);
             });
         }
 
